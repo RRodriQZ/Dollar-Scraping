a437f8982e5667708d52f5fb3f7e8cb4a5a85b42
RRodriQZ <rrquispezabala@gmail.com>
2021-08-08 02:06:26 -0300

    Implemented the setup configuration

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from sheets.report import check_exists_excel_file, update_sheet_row
 from functions.functions import get_scraping_values_format_tuple
+from sheets.report import update_sheet_row
 from scrap.bloomberg_scrap import Bloomberg
 from scrap.dollar_scrap import Dollar
 from scrap.cme_scrap import Cme
 
 
 def main() -> None:
-    # Get the values ​​of the Scrapings
+    # Get the values of the Scrapings
     dollar_scrap = Dollar().get_data_from_pages()
     cme_scrap = Cme().get_data_from_pages()
     bloomberg_scrap = Bloomberg().get_data_from_pages()
 
     # Append the Scrapings in a tuple
     scraping_tuple = get_scraping_values_format_tuple(
-        dollar_json = dollar_scrap,
-        cme_json = cme_scrap,
-        bloomberg_json = bloomberg_scrap
+        dollar_json=dollar_scrap,
+        cme_json=cme_scrap,
+        bloomberg_json=bloomberg_scrap
     )
 
     # Verify that the report exists -> update the Excel row
